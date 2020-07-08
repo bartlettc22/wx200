1c138b2090db2c271d2e7bf3ece2f6c345de48bc
Chris Bartlett <bartlettc@gmail.com>
2020-07-08 16:29:46 -0600

    testing build

--- Dockerfile
+++ Dockerfile
@@ -2,17 +2,15 @@
 FROM golang:1.14 as builder
 
 ARG VERSION=
-ARG TARGETPLATFORM=
-ARG BUILDPLATFORM=
+ARG GOARCH=
+ARG GOOS=
 
 WORKDIR /go/src/github.com/bartlettc22/wx200/
 COPY ./ .
 
-RUN echo "I am running on $BUILDPLATFORM, building for $TARGETPLATFORM"
-
 RUN CGO_ENABLED=0 GOOS=${GOOS} GOARCH=${GOARCH} go build -mod vendor -ldflags "-s -w -X github.com/bartlettc22/wx200/main.Version=${VERSION}" -v -a -o bin/wx200 .
 
-FROM ${ARCH}alpine:3.12
+FROM alpine:3.12
 
 COPY --from=builder /go/src/github.com/bartlettc22/wx200/bin/wx200 /usr/bin
 
--- scripts/build.sh
+++ scripts/build.sh
@@ -4,60 +4,114 @@ set -eo pipefail
 set -o xtrace
 
 VERSION=${1:-master}
-OS=${2:-linux}
-ARCH=${3:-amd64}
+PLATFORMS=${2:-"linux/amd64"}
+PUBLISH=${3:-"false"}
 DOCKER_REPO="bartlettc/wx200"
 
+# archives the binary in the bin/ directory
+function archive {
+    VERSION=${1}
+    OS=${2}
+    ARCH=${3}
+    cd bin
+    if [ "${OS}" == "windows" ]; then
+        mv wx200 wx200.exe
+        zip wx200-${VERSION}-${OS}-${ARCH}.zip wx200.exe
+        rm -rf wx200.exe    
+    else 
+        tar -cvzf wx200-${VERSION}-${OS}-${ARCH}.tar.gz wx200
+        rm -rf wx200
+    fi
+    cd -
+}
+
+if [ "$PUBLISH" == "true" ]; then
+    echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+fi
 
 # docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 # docker buildx create --name xbuilder --use
-# Don't build an ARM Windows binary
-# if [[ "${OS}" == "windows" && "${ARCH}" == "arm" ]]; then
-#     exit 0
-# fi
 
-# Directory to house our binaries
+LINUX_PLATFORMS=()
+OTHER_PLATFORMS=()
+for PLATFORM in $(echo ${PLATFORMS} | sed "s/,/ /g")
+do
+    if [ "${PLATFORM%/*}" == "linux" ]; then
+        LINUX_PLATFORMS+=("${PLATFORM}")
+    else 
+        OTHER_PLATFORMS+=("${PLATFORM}")
+    fi
+done
+
 mkdir -p bin
+rm -rf bin/*
 
-# Build the container
-# docker build \
-#     --build-arg VERSION=${VERSION} \
-#     --build-arg GOOS=${OS} \
-#     --build-arg GOARCH=${ARCH} \
-#     -t ${DOCKER_REPO}:${VERSION}-${OS}-${ARCH} \
-#     ./
-docker buildx build --platform=linux/amd64,linux/arm64 -t bartlettc/wx200:latest --push . # --progress plain
-# docker buildx -h
-# docker buildx build \
-#      --progress plain \
-#     --platform=linux/amd64,linux/arm64 \
-#     -t ${DOCKER_REPO}:${VERSION} \
-#     .
+# Build and extract binaries
+if [ ${#LINUX_PLATFORMS[@]} > 0 ]; then
 
-docker image ls -a
-ls -al ./bin/test
+    # Build and push linux multiarch images
+    PUSH=""
+    if "{PUBLISH}" == "true" ]; then
+      PUSH="--push"
+    fi
+    docker buildx build \
+        --build-arg VERSION=${VERSION} \
+        --platform=$(IFS=, ; echo "${LINUX_PLATFORMS[*]}") \
+        --progress plain \
+        ${PUSH} \
+        -t ${DOCKER_REPO}:${VERSION} \
+        .
 
-#docker run test
-# docker manifest create bartlettc/wx200:test \
-# --amend bartlettc/wx200:test-amd64 \
-# --amend bartlettc/wx200:test-arm
+    # Extract the binaries from the containers
+    if "{PUBLISH}" == "true" ]; then 
+        for PLATFORM in ${LINUX_PLATFORMS[@]}
+        do
+            OS=${PLATFORM%/*}
+            ARCH=${PLATFORM#*/}
+            echo "${OS}/${ARCH}"
+            # ARCH=${PLATFORM#"/"}
+            docker pull --platform ${PLATFORM} ${DOCKER_REPO}:${VERSION}
+            docker run \
+                --rm -t \
+                --entrypoint "" \
+                --name wx200-build-${OS}-${ARCH} \
+                -v $(pwd)/bin:/wx200-bin ${DOCKER_REPO}:${VERSION} \
+                sh -c "chmod 777 /usr/bin/wx200 && cp /usr/bin/wx200 /wx200-bin"
 
-# Extract the binary from the container
-# docker run \
-#     --rm \
-#     --entrypoint "" \
-#     --name wx200-build \
-#     -v $(pwd)/bin:/wx200-bin ${DOCKER_REPO}:${VERSION}-${OS} \
-#     sh -c "cp /usr/bin/wx200 /wx200-bin"
+            # Archive the binary
+            archive ${VERSION} ${OS} ${ARCH}
+        done
+    fi
+fi
 
-# # Zip up the binary
-# cd bin
-# if [[ "${OS}" == "linux" ]]; then
-#     tar -cvzf wx200-${VERSION}-${OS}-${ARCH}.tar.gz wx200
-#     rm wx200
-# elif [[ "${OS}" == "windows" ]]; then
-#     mv wx200 wx200.exe
-#     zip wx200-${VERSION}-${OS}-${ARCH}.zip wx200.exe
-#     rm wx200.exe
-# fi
-# cd ..
+if [ ${#OTHER_PLATFORMS[@]} > 0 ]; then
+
+    
+    for PLATFORM in $(echo ${OTHER_PLATFORMS[@]} | sed "s/,/ /g")
+    do
+        OS=${PLATFORM%/*}
+        ARCH=${PLATFORM#*/}
+
+        # Use docker to build the windows binary
+        docker build \
+        --build-arg VERSION=${VERSION} \
+        --build-arg GOOS=${OS} \
+        --build-arg GOARCH=${ARCH} \
+        -t ${DOCKER_REPO}:build-${OS}-${ARCH} \
+        .
+
+        if "{PUBLISH}" == "true" ]; then 
+
+            # Run the container to extract the binary
+            docker run \
+            --rm \
+            --entrypoint "" \
+            --name wx200-build-${OS}-${ARCH} \
+            -v $(pwd)/bin:/wx200-bin ${DOCKER_REPO}:build-${OS}-${ARCH} \
+            sh -c "cp /usr/bin/wx200 /wx200-bin"
+
+            # Archive the binary
+            archive ${VERSION} ${OS} ${ARCH}
+        fi
+    done
+fi
